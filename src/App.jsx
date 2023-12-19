62be61757a34cec48ff1bacf717493b5f9ab6677
klvnnn <klvnnnyatiga@gmail.com>
2023-12-19 15:56:16 +0700

    First upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+function App() {
+  // Props
+  const footerText = "Customise Your Product"
+  // State
+  const [getNavbar, SetNavbar] = useState("");
+  const changeNavbar = () => {
+    SetNavbar("Contact");
+  };
+
+  return (
+    <>
+    <Navbar navbarValue={getNavbar}/>
+    <button onClick={changeNavbar}>Click</button>
+    <Footer footerText={footerText}/>
+    </>
+  );
+}
+
+export default App;
